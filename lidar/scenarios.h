a8d929c248742ec65b10cf7e76681643a912bd77
dsonyy <dsonyyy@gmail.com>
2020-12-07 00:15:22 +0100

    Save series scenario.

---
+++ lidar/scenarios.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <chrono>
+#include "app.h"
+
+class Scenario {
+public:
+	virtual bool update(uint8_t * render_buffer, Cloud& cloud) = 0;
+	inline virtual int get_status() const { return status_; }
+protected:
+	int status_;
+};
+
+class SaveSeriesScenario
+	: public Scenario {
+public:
+	SaveSeriesScenario(const std::string& dir);
+	~SaveSeriesScenario();
+
+	virtual bool update(uint8_t* render_buffer, Cloud& cloud);
+private:
+	std::string filename_;
+	std::ofstream file_;
+	std::size_t frame_counter_;
+	std::chrono::steady_clock::time_point time_begin_;
+};
