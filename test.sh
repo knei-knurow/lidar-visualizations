8490899f2b1eb14eb371bb5c256e6c24ea2cca6b
dsonyy <dsonyyy@gmail.com>
2020-12-20 16:41:46 +0100

    Basic compilation using bash script/

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 g++ -Wall \
+	-DUSING_SFML \
+	-DUSING_RPLIDAR \
+	-std=c++17 \
 	-I3rdparty/rplidar_sdk/sdk/sdk/include \
 	-I3rdparty/rplidar_sdk/sdk/sdk/src \
-	test.cpp \
+	-Isfml/include \
+	lidar/cloud.cpp lidar/cloud-grabbers.cpp lidar/cloud-writers.cpp lidar/scenarios.cpp lidar/guis.cpp lidar/app.cpp lidar/main.cpp \
 	-L3rdparty/rplidar_sdk/sdk/output/Linux/Release \
+	-Lsfml/lib \
 	-pthread \
 	-lrplidar_sdk \
 	-lsfml-system -lsfml-window -lsfml-graphics \
-	-o test
+	-o asdfasdf
