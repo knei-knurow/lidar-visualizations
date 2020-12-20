bde63151ffa08bcc6020efa31e130de6777c5a13
dsonyy <dsonyyy@gmail.com>
2020-12-20 19:39:57 +0100

    Remove lidar directory and update *.props files.

--- lidar/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "app.h"
-
-int main(int argc, char** argv) {
-  std::vector<std::string> args;
-  for (int i = 1; i < argc; i++) {
-    args.push_back(std::string(argv[i]));
-  }
-  return App(args).run();
-}
